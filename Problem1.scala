cc510fbadf75e2b55a87a5c4bc39851180c53a2c
mahesh chand <shivangi.gupta@knoldus.in>
2017-04-03 22:18:05 +0530

    adding question with solution

--- Problem1.scala
+++ Problem1.scala
@@ -1,3 +1,11 @@
+/*
+(*) Find the last element of a list.
+    Example:
+
+    scala> last(List(1, 1, 2, 3, 5, 8))
+    res0: Int = 8
+*/
+
 class Problem1 {
 
  def lastElement(l:List[Int]):Int = {
--- Problem2.scala
+++ Problem2.scala
@@ -1,3 +1,10 @@
+/*
+  (*) Find the last but one element of a list.
+    Example:
+
+    scala> penultimate(List(1, 1, 2, 3, 5, 8))
+    res0: Int = 5
+*/
 class Problem2 {
 
  def secondLastElement(l:List[Int]):Int = {
