0ea76b59532bf0dab715293d89236e47c4eb34e0
mahesh chand <shivangi.gupta@knoldus.in>
2017-04-03 22:34:12 +0530

    question of problem 4

--- Problem4.scala
+++ Problem4.scala
@@ -1,3 +1,10 @@
+/*
+(*) Find the number of elements of a list.
+    Example:
+
+    scala> length(List(1, 1, 2, 3, 5, 8))
+    res0: Int = 6
+*/
 class Problem4 {
 
 def length(l:List[Int]):Int = {
