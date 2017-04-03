b331504ee4cea328cb1ae225c0757158b128d639
mahesh chand <shivangi.gupta@knoldus.in>
2017-04-03 22:15:37 +0530

    Solution of Problem 1 and 2

---
+++ Problem1.scala
@@ -0,0 +1,27 @@
+class Problem1 {
+
+ def lastElement(l:List[Int]):Int = {
+
+      def last(l:List[Int]):Int = {
+
+      l match {
+
+      case head::Nil => head
+      case _::tail => last(tail)
+      case Nil => throw new NoSuchElementException
+      }
+      }
+      last(l)
+      }
+}
+
+object Problem1 extends App {
+
+  val pObj = new Problem1
+  val list = List(9,1,4,5,6,2,3)
+  println("Using pattern matching "+pObj.lastElement(list))
+  println("Using inbuilt function "+ list.last)
+
+}
+
+
