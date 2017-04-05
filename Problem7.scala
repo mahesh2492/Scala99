e1f96180b72c6cb84490a4b0a37f2b859372ad7f
mahesh chand <shivangi.gupta@knoldus.in>
2017-04-05 22:31:48 +0530

    Probelm 7 solution

--- Problem7.scala
+++ Problem7.scala
@@ -1,3 +1,10 @@
+/* 
+ (**) Flatten a nested list structure.
+    Example:
+
+    scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+    res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+*/
 class Problem7 {
 def flatten(list:List[Any]):List[Any] = {
  list flatMap {
