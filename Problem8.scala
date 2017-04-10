433434b38e02a5e654c2a801979d1f950307061e
mahesh chand <shivangi.gupta@knoldus.in>
2017-04-10 11:44:29 +0530

    solutions

---
+++ Problem8.scala
@@ -0,0 +1,26 @@
+/*
+(**) Eliminate consecutive duplicates of list elements.
+    If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+
+    Example:
+
+    scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+    res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e) 
+*/
+
+class Problem8 {
+def compress(list:List[Symbol]):List[Symbol]= {
+  list match {
+   case Nil => Nil
+   case head::tail => head :: compress(tail.dropWhile(_ == head))
+  }
+}
+}
+
+object Problem8 extends App {
+
+  val pObj = new Problem8
+  val list =  List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+  println("Original List: "+list)
+  println("Compressed list "+ pObj.compress(list))
+}
