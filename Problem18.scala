0640296858fcf7e2eb5c638d23ff65d457971a65
mahesh chand <mahesh.kndpl@gmail.com>
2017-04-18 17:05:58 +0530

    Problem18 solution

---
+++ Problem18.scala
@@ -0,0 +1,7 @@
+object Problem18 extends App {
+  val list = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
+  val start = 3
+  val end = 7
+  println("Sliced List of given list: " + list.slice(start,end))
+  
+}
